69ecc8e093c91c72e147be5654d9932d3ca556d6
Sechang OH <twinf1004@hufs.ac.kr>
2022-06-01 02:24:20 +0900

    Update 9. 오류 찾기 2.py

--- [3주차]/9. 오류 찾기 2.py
+++ [3주차]/9. 오류 찾기 2.py
@@ -1,1 +1,13 @@
 # 서술형 문제
+
+# print("you" "need" "python")
+# youneedpython
+
+# print("you"+"need"+"python")
+# youneedpython
+
+# print("you", "need", "python")   # 콤마가 있는 경우 공백이 삽입되어 더해진다.
+# you need python
+
+# print("".join(["you", "need", "python"]))
+# youneedpython
