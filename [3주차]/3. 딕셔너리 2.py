a88f665dd04a3e93f4ef97d1f31874c44d3beec8
Sechang OH <twinf1004@hufs.ac.kr>
2022-06-01 01:56:10 +0900

    Create 3. 딕셔너리 2.py

---
+++ [3주차]/3. 딕셔너리 2.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+a = {'A':90, 'B':80, 'C':70}
+result = a.pop('B')
+# print(a)            # {'A':90, 'C':70} 출력
+print(result)       # 80 출력
