a6d91cc58631b15916e7869d7fdd9b4bf6992326
Sechang OH <twinf1004@hufs.ac.kr>
2022-06-01 02:06:47 +0900

    Create 1. 암호 검사 프로그램.py

---
+++ [4주차](기말고사 대비)/[기본 틀]/1. 암호 검사 프로그램.py
@@ -0,0 +1,10 @@
+def gainRootRight():
+  PASSWORD = 'hufs2020'
+  MAX_TRIAL = 3
+  
+# 여기에 필요한 코드를 추가하시오(코드를 입력하시오)
+
+if gainRootRight():
+  print('관리자 권한을 획득하였습니다.')
+else:
+  print('당신은 권한이 없습니다.')
