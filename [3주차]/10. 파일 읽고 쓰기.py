d431513f933acad206f284c41ce9d002f385449e
Sechang OH <twinf1004@hufs.ac.kr>
2022-06-01 01:59:08 +0900

    Create 10. 파일 읽고 쓰기.py

---
+++ [3주차]/10. 파일 읽고 쓰기.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+f1 = open("test.txt", 'w')
+f1.write("Life is too short!")
+f1.close() # 열린 파일 객체를 닫는다.
+
+f2 = open("test.txt", 'r')
+print(f2.read())
+f2.close()
